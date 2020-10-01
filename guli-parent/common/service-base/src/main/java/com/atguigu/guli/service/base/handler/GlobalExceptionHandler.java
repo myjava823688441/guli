14f62f22e18022ac3374648a5ca67081253f47ae
mmp <mmp@163.com>
2020-10-01 22:53:00 +0800

    初始化项目

---
+++ guli-parent/common/service-base/src/main/java/com/atguigu/guli/service/base/handler/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.atguigu.guli.service.base.handler;
+
+import com.atguigu.guli.common.base.result.R;
+import com.atguigu.guli.common.base.util.ExceptionUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public R handleException(Exception e){
+        log.error(ExceptionUtils.getMessage(e));
+        return R.error();
+    }
+}
